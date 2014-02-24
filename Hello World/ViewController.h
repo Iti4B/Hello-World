ae53dcb3fbf0ba9c2a957c3881062c362da1a39f
Admin <admin@Andreas-iMac-2.local>
2014-02-24 09:45:46 +0100

    Initial Commit

---
+++ Hello World/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Hello World
+//
+//  Created by Admin on 24/02/14.
+//  Copyright (c) 2014 Admin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
